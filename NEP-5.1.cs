bcdde0897ab4cb489e361014459c486c3b81e921
Sinepitiss <gatis.dauksa@andit.lv>
2018-03-27 20:23:03 +0300

    Added Notify if Operation is not found

--- NEP-5.1.cs
+++ NEP-5.1.cs
@@ -82,7 +82,7 @@ namespace Neo.SmartContract
                     return BalanceOf(account);
                 }
             }
-            return false;
+            return NotifyErrorAndReturnFalse("Operation not found");
         }
 
         /// <summary>
